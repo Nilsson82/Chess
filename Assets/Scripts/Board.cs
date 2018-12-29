5e28e8267d73d4ea702065cb103c0763b629c254
Nilsson82 <Anders858@gmail.com>
2018-12-29 14:40:33 +0100

    Created board and piece

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Board : MonoBehaviour
+{
+    public GameObject mCellPrefab;
+
+    [HideInInspector]
+    public Cell[,] mAllCells = new Cell[8, 8];
+
+    public void Create()
+    {
+        for (int y = 0; y < 8; y++)
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                // Create the cell
+                GameObject newCell = Instantiate(mCellPrefab, transform);
+
+                // Position
+                RectTransform rectTransform = newCell.GetComponent<RectTransform>();
+                rectTransform.anchoredPosition = new Vector2((x * 100) + 50, (y * 100) + 50);
+
+                //Setup
+                mAllCells[x, y] = newCell.GetComponent<Cell>();
+                mAllCells[x, y].Setup(new Vector2Int(x, y), this);
+
+            }
+        }
+
+        for (int x = 0; x < 8; x += 2)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                //Offset for every other line
+                int offset = (y % 2 != 0) ? 0 : 1;
+                int finalX = x + offset;
+
+                // Color
+                mAllCells[finalX, y].GetComponent<Image>().color = new Color32(230, 220, 187, 255);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Pieces/BasePiece.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public abstract class BasePiece : EventTrigger
+{
+    [HideInInspector]
+    public Color mColor = Color.clear;
+    public bool mIsFirstMove = true;
+
+    protected Cell mOriginalCell = null;
+    protected Cell mCurrentCell = null;
+
+    protected RectTransform mRectTransform = null;
+    protected PieceManager mPieceManager;
+
+    public virtual void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager)
+    {
+        mPieceManager = newPieceManager;
+
+        mColor = newTeamColor;
+        GetComponent<Image>().color = newSpriteColor;
+        mRectTransform = GetComponent<RectTransform>();
+    }
+
+    public virtual void Place(Cell newCell)
+    {
+        // Cell stuff
+        mCurrentCell = newCell;
+        mOriginalCell = newCell;
+        mCurrentCell.mCurrentPiece = this;
+
+        // Object stuff
+        transform.position = newCell.transform.position;
+        gameObject.SetActive(true);
+    }
+}
