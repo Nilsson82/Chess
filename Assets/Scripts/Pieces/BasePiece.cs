4c2d8ad72260166940fad72f8514a0b4738a1037
Nilsson82 <Anders858@gmail.com>
2018-12-29 16:17:46 +0100

    Started added movement of pieces

--- Assets/Scripts/Pieces/BasePiece.cs
+++ Assets/Scripts/Pieces/BasePiece.cs
@@ -7,13 +7,17 @@ public abstract class BasePiece : EventTrigger
 {
     [HideInInspector]
     public Color mColor = Color.clear;
-    public bool mIsFirstMove = true;
+    //public bool mIsFirstMove = true;
 
     protected Cell mOriginalCell = null;
     protected Cell mCurrentCell = null;
 
     protected RectTransform mRectTransform = null;
     protected PieceManager mPieceManager;
+
+    protected Vector3Int mMovement = Vector3Int.one;
+    protected List<Cell> mHighlightedCells = new List<Cell>();
+
 
     public virtual void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager)
     {
@@ -35,4 +39,102 @@ public abstract class BasePiece : EventTrigger
         transform.position = newCell.transform.position;
         gameObject.SetActive(true);
     }
+
+    private void CreateCellPath(int xDircetion, int yDirection, int movement)
+    {
+        // Target possition
+        int currentX = mCurrentCell.mBoardPosition.x;
+        int currentY = mCurrentCell.mBoardPosition.y;
+
+        
+
+        // Check each cell
+        for (int i = 1; i <= movement; i++)
+        {
+            currentX += xDircetion;
+            currentY += yDirection;
+
+            // TODo: Get the state of target cell
+
+            // Add to list
+            mHighlightedCells.Add(mCurrentCell.mBoard.mAllCells[currentX, currentY]);
+
+        }
+
+    }
+
+    protected virtual void CheckPathing()
+    {
+        // Horizontal
+        CreateCellPath(1, 0, mMovement.x);
+        CreateCellPath(-1, 0, mMovement.x);
+
+        // Vertical
+        CreateCellPath(0, 1, mMovement.y);
+        CreateCellPath(0, -1, mMovement.y);
+
+        // Upper diagonal
+        CreateCellPath(1, 1, mMovement.z);
+        CreateCellPath(-1, 1, mMovement.z);
+
+        // Lower diagonal
+        CreateCellPath(-1, -1, mMovement.z);
+        CreateCellPath(1, -1, mMovement.z);
+    }
+
+    protected void ShowCells()
+    {
+
+
+        Debug.Log("Size: " + mHighlightedCells.Count);
+
+        foreach (Cell cell in mHighlightedCells)
+        {
+            Debug.Log("Foreath: ");
+            Debug.Log("Enable: " + cell.mOutlineImage.enabled);
+            cell.mOutlineImage.enabled = true;
+        }
+        Debug.Log("End: ");
+    }
+
+    protected void ClearCells()
+    {
+        foreach (Cell cell in mHighlightedCells)
+        {
+            cell.mOutlineImage.enabled = false;
+        }
+        mHighlightedCells.Clear();
+    }
+
+    #region Events
+
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        base.OnBeginDrag(eventData);
+
+        //Test for cells
+        CheckPathing();
+
+        // Show valid cells
+        ShowCells();
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+
+        // Follow pointer
+        transform.position += (Vector3)eventData.delta;
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        base.OnEndDrag(eventData);
+
+        // Hide
+        ClearCells();
+    }
+
+    #endregion
 }
+
